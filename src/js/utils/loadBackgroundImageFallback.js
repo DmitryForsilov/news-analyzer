541da06d19b9689c89dd633c8fb199095d5de60b
DmitryForsilov <tpadtpad@yandex.ru>
2020-08-10 20:52:09 +0300

    fix loadBackgroundImageFallback

--- src/js/utils/loadBackgroundImageFallback.js
+++ src/js/utils/loadBackgroundImageFallback.js
@@ -1,5 +1,3 @@
-const urlToDefaultImage = '../../images/default-news-image.jpg';
-
 export default (imageWrapper, urlToImage) => {
   let loaded = false;
   const image = new Image();
@@ -13,8 +11,6 @@ export default (imageWrapper, urlToImage) => {
   setTimeout(() => {
     if (loaded) {
       imageWrapper.style['background-image'] = `url(${urlToImage})`;
-    } else {
-      imageWrapper.style['background-image'] = `url(${urlToDefaultImage})`;
     }
   }, 200);
 };
