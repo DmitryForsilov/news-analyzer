6b9ff757f2521f7d3d987116cdfee7ce90edcc04
DmitryForsilov <tpadtpad@yandex.ru>
2020-08-14 15:30:32 +0300

    fix baseUrl for newsApi config

--- src/js/constants/NEWS_API_CONFIG.js
+++ src/js/constants/NEWS_API_CONFIG.js
@@ -1,7 +1,7 @@
 export default {
   baseUrl: NODE_ENV === 'development' // eslint-disable-line no-undef
     ? 'https://newsapi.org'
-    : 'https://nomoreparties.co', // 'https://praktikum.tk/news'
+    : 'https://nomoreparties.co/news', // 'https://praktikum.tk/news'
   endpoint: 'v2/everything',
   options: {
     apiKey: '26a8a91802054a24bd726b2f2c9765c6',
