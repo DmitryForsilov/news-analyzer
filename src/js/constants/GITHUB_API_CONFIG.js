1f63f6c5a20b747f202ffd4b66a125cb6bfedf84
DmitryForsilov <tpadtpad@yandex.ru>
2020-10-05 12:45:11 +0300

    changing repo name for gh api

--- src/js/constants/GITHUB_API_CONFIG.js
+++ src/js/constants/GITHUB_API_CONFIG.js
@@ -2,7 +2,7 @@ export default {
   baseUrl: 'https://api.github.com',
   reposPath: 'repos',
   owner: 'DmitryForsilov',
-  repo: 'NewsAnalyzer',
+  repo: 'news-analyzer',
   commitsPath: 'commits',
   options: {
     per_page: 20,
